78a0b70d0650abeaf1067c35535f15ec1ac45634
sannimdev <inegg.apps@gmail.com>
2021-08-31 07:08:43 +0900

    에러 페이지 정의 방법

--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        수업에서는 layouts, middleware 폴더가 보이는데 현재는... 폴더 구조가 강의와 다르게 구성된다
-        (layouts, middleware 폴더 없이 프로젝트 생성됨)
-        <a href="https://www.inflearn.com/questions/284061">여기</a> 참조
-    </div>
-</template>
---
+++ layouts/error.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>404 페이지</h1>
+        <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+</template>
