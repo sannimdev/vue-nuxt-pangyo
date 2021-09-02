212f88914937c47f144e39efadfea4c141073474
sannimdev <inegg.apps@gmail.com>
2021-09-02 20:22:22 +0900

    axios 설치 및 API 호출

--- pages/main.vue
+++ pages/main.vue
@@ -6,7 +6,14 @@
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+    async created() {
+        const response = await axios.get('http://localhost:3000/products');
+        console.log(response);
+    },
+};
 </script>
 
 <style scoped></style>
