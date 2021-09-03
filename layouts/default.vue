4e5987f1fc69fd6a8b93b3ddfcdc7d514c2eb3a5
sannimdev <inegg.apps@gmail.com>
2021-09-03 21:24:20 +0900

    상품 상세 정보 조회 기능 개발

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,11 +8,7 @@
 </template>
 
 <script>
-export default {
-    created() {
-        console.log(this.$route);
-    },
-};
+export default {};
 </script>
 
 <style></style>
--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
         <h1>상세 페이지</h1>
+        <div>
+            {{ product }}
+            <img :src="product.imageUrl" :alt="product.name" />
+            <p>name: {{ product.name }}</p>
+            <p>price: {{ product.price }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import { fetchProductById } from '@/api';
 export default {
-    created() {
-        console.log(this.$route);
+    async asyncData({ params }) {
+        // asyncData는 페이지를 진입하기 전에  사용하므로 this를 사용할 수가 없다.
+        const response = await fetchProductById(params.id);
+        const product = response.data;
+        return { product };
     },
+    created() {},
 };
 </script>
 
