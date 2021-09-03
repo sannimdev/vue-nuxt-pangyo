70a3a5f938e31949dbe48d6ab5f301474faf24fe
sannimdev <inegg.apps@gmail.com>
2021-09-03 21:24:30 +0900

    상품 상세 페이지 레이아웃 정리 및 전역 스타일 시트 설정하기

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        <h1>상세 페이지</h1>
-        <div>
-            {{ product }}
-            <img :src="product.imageUrl" :alt="product.name" />
-            <p>name: {{ product.name }}</p>
-            <p>price: {{ product.price }}</p>
+        <div class="container">
+            <div class="main-panel">
+                <img class="product-image" :src="product.imageUrl" :alt="product.name" />
+            </div>
+            <div class="side-panel">
+                <p class="name">{{ product.name }}</p>
+                <p class="price">{{ product.price }}</p>
+                <!-- <button type="button" @click="addToCart">Add to Cart</button> -->
+            </div>
         </div>
     </div>
 </template>
@@ -13,8 +16,9 @@
 <script>
 import { fetchProductById } from '@/api';
 export default {
+    // context는 넉스트 프레임워크 전반에 걸쳐 공용으로 사용되는 속성으로써 플러그인, 미들웨어 드으이 속성에도 접근할 수 있음.
     async asyncData({ params }) {
-        // asyncData는 페이지를 진입하기 전에  사용하므로 this를 사용할 수가 없다.
+        // asyncData는 페이지를 진입하기 전이라서 페이지 컴포넌트를 가리키고자 할 때는 this 키워드를 사용할 수가 없다.
         const response = await fetchProductById(params.id);
         const product = response.data;
         return { product };
@@ -23,4 +27,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0;
+}
+.product-image {
+    width: 500px;
+    height: 375px;
+}
+.side-panel {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 220px;
+    text-align: center;
+    padding: 0 1rem;
+}
+</style>
