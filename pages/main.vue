ce575d2edec4c954d04ecfe7897060ae6b7d8d1a
sannimdev <inegg.apps@gmail.com>
2021-09-03 21:23:03 +0900

    상품 목록 표시 기능 구현

--- pages/main.vue
+++ pages/main.vue
@@ -3,7 +3,13 @@
         <h1>홈페이지</h1>
         <p>메인페이지입니다</p>
         <div>
-            {{ products }}
+            <ul>
+                <li v-for="product in products" :key="product.id">
+                    <img :src="product.imageUrl" :alt="product.name" />
+                    <p>{{ product.name }}</p>
+                    <p>{{ product.price }}</p>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -16,7 +22,10 @@ export default {
         // VueRoute에서 beforeEnter 사이클(즉, 라우터에 진입하기 전 시점)에서 동작하는 것과 같다고 할 수 있다.
         // asyncData 메서드는 pages 이하 컴포넌트에서만 적용할 수 있는 것에 주의한다.
         const response = await axios.get('http://localhost:3000/products');
-        const products = response.data;
+        const products = response.data.map((item) => ({
+            ...item,
+            imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+        }));
 
         return {
             products,
