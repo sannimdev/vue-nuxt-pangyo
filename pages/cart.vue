b0348bfaea71e3997f9b23ca1a6222906b0c26e7
sannimdev <inegg.apps@gmail.com>
2021-09-07 20:15:39 +0900

    장바구니 페이지의 장바구니 목록 조회 기능

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <h1>카트</h1>
+        <h1>카트 페이지</h1>
+        <div>
+            <ul>
+                <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id">
+                    <img :src="cartItem.imageUrl" :alt="cartItem.name" />
+                    <p>{{ cartItem.name }}</p>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button>구매하기</button>
+        </div>
     </div>
 </template>
 
