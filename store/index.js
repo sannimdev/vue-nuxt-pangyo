a603fc6e95889b5e144507b122cd59c67f1bb661
sannimdev <inegg.apps@gmail.com>
2021-09-09 09:35:28 +0900

    NuxtServerInit 적용 및 특성 안내

--- store/index.js
+++ store/index.js
@@ -8,11 +8,7 @@ export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS';
 
 export const mutations = {
     addCartItem(state, cartItem) {
-        const newCartItem = {
-            ...cartItem,
-            imageUrl: `${cartItem.imageUrl}?random=${Math.random()}`,
-        };
-        state.cartItems.push(newCartItem);
+        state.cartItems.push(cartItem);
     },
     setCartItems(state, cartItems) {
         state.cartItems = cartItems;
@@ -23,6 +19,23 @@ export const actions = {
     // vue+ts에서 mutations와 actions의 속성은 ts에서 별도로 상수화시켜 import 하는 방식에서 다음과 같이 대문자 스네이크 케이스를 사용하는 것이 유리하다.
     async [FETCH_CART_ITEMS]({ commit }) {
         const { data } = await fetchCartItems();
-        commit('setCartItems', data);
+        commit(
+            'setCartItems',
+            data.map((item) => ({
+                ...item,
+                imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+            }))
+        );
+    },
+    async nuxtServerInit(storeContext, nuxtContext) {
+        await storeContext.dispatch(FETCH_CART_ITEMS);
+        // const { data } = await fetchCartItems();
+        // storeContext.commit(
+        //     'setCartItems',
+        //     data.map((item) => ({
+        //         ...item,
+        //         imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+        //     }))
+        // );
     },
 };
